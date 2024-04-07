014e0f7f2f520c02cf05018c3c92f4bfd072c183
Patrick Reis <patrickbernardor@gmail.com>
2024-04-07 00:31:52 -0300

    chore: update base64 max length

--- Impar.Infra/Migrations/20240407031702_ChangeBaseMaxLength.Designer.cs
+++ Impar.Infra/Migrations/20240407031702_ChangeBaseMaxLength.Designer.cs
@@ -3,6 +3,7 @@ using Impar.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Impar.Infra.Migrations
 {
     [DbContext(typeof(EntityContext))]
-    partial class EntityContextModelSnapshot : ModelSnapshot
+    [Migration("20240407031702_ChangeBaseMaxLength")]
+    partial class ChangeBaseMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
