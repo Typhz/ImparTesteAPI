454de0c7cce0b0fc08590b500080553d429ba892
Patrick Reis <patrickbernardor@gmail.com>
2024-04-11 01:04:13 -0300

    feat: add service integration

--- src/Impar.API/Controllers/CarController.cs
+++ src/Impar.API/Controllers/CarController.cs
@@ -1,8 +1,6 @@
-﻿using Impar.Domain.Models;
-using Impar.Infra.Context;
-using ImparTesteAPI.DTOs;
+﻿using ImparTesteAPI.DTOs;
+using ImparTesteAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace ImparTesteAPI.Controllers;
 
@@ -11,80 +9,44 @@ namespace ImparTesteAPI.Controllers;
 public class CarController : ControllerBase
 {
 
-	private readonly EntityContext _context;
-
-	public CarController(EntityContext context)
+	private readonly ICarService _carService;
+	public CarController(ICarService carService)
 	{
-		_context = context;
+		_carService = carService;
 	}
 
-	// GET
 	[HttpGet]
-	public async Task<ActionResult> Index()
+	public async Task<ActionResult> Index(int pageNumber = 1, int pageSize = 10, string? searchTerm = null)
 	{
-		var cars = await _context.Cars.ToListAsync();
-
-		return Ok(cars);
+		var carDtos = await _carService.GetAllCarsAsync(pageNumber, pageSize, searchTerm);
+		return Ok(carDtos);
 	}
 
 	[HttpGet("{id}")]
 	public async Task<ActionResult> Get(int id)
 	{
-		var car = await _context.Cars.FindAsync(id);
-
-		if (car == null)
-		{
-			return NotFound();
-		}
-
-		var carDto = new CarDto
-		{
-			Id = car.Id,
-			Name = car.Name,
-			Status = car.Status,
-			Photo = new PhotoDto
-			{
-				Base64 = car.Photo.Base64
-			}
-		};
-
+		var carDto = await _carService.GetCarByIdAsync(id);
 		return Ok(carDto);
 	}
 
 	[HttpPost]
-	public async Task<ActionResult> Create([FromForm] CarDto carDto)
+	public async Task<ActionResult> Create([FromForm] CarCreateDto carCreateDto)
 	{
+		var carDto = await _carService.CreateCarAsync(carCreateDto);
+		return Ok(carDto);
+	}
 
-		var car = new Car
-		{
-			Name = carDto.Name,
-			Status = carDto.Status,
-		};
+	[HttpPut("{id}")]
+	public async Task<ActionResult> Update(int id, [FromForm] CarUpdateDto carUpdateDto)
+	{
+		await _carService.UpdateCarAsync(id, carUpdateDto);
+		return Ok();
+	}
 
-		_context.Cars.Add(car);
-		await _context.SaveChangesAsync();
-
-		if (carDto.ImageFile != null && carDto.ImageFile.Length > 0)
-		{
-			using var ms = new MemoryStream();
-			await carDto.ImageFile.CopyToAsync(ms);
-			var fileBytes = ms.ToArray();
-			string base64String = Convert.ToBase64String(fileBytes);
-
-			var photo = new Photo
-			{
-				Base64 = base64String,
-				CarId = car.Id
-			};
-
-			_context.Photos.Add(photo);
-			await _context.SaveChangesAsync();
-
-			car.PhotoId = photo.Id;
-			_context.Cars.Update(car);
-			await _context.SaveChangesAsync();
-		}
-
-		return CreatedAtAction("Get", new { id = car.Id }, car);
+	[HttpDelete("{id}")]
+	public async Task<ActionResult> Delete(int id)
+	{
+		await _carService.DeleteCarAsync(id);
+		return Ok();
 	}
 }
