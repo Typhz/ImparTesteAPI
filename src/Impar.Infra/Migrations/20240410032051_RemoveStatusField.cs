eb4c802bdcfa4f4fe5bceafa69eaa92c4b7ec8e0
Patrick Reis <patrickbernardor@gmail.com>
2024-04-11 01:01:55 -0300

    chore: add publish settings

---
+++ src/Impar.Infra/Migrations/20240410032051_RemoveStatusField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Impar.Infra.Migrations
+{
+    public partial class RemoveStatusField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "cars");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "cars",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
