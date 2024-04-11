a2fe2d2403b7057e0632b82fdc22e019fd8a79c6
Patrick Reis <patrickbernardor@gmail.com>
2024-04-11 01:03:46 -0300

    feat: create car service interface

---
+++ src/Impar.API/Services/Interfaces/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using ImparTesteAPI.DTOs;
+
+namespace ImparTesteAPI.Services.Interfaces;
+
+public interface ICarService
+{
+	Task<List<CarReadDto>> GetAllCarsAsync(int pageNumber = 1, int pageSize = 10 , string? searchTerm = null);
+	Task<CarReadDto> GetCarByIdAsync(int id);
+	Task<CarReadDto> CreateCarAsync(CarCreateDto carCreateDto);
+	Task UpdateCarAsync(int id, CarUpdateDto carUpdateDto);
+	Task DeleteCarAsync(int id);
+}
